3acc30dc60b7386f56ed06119d40b57084b93b52
kezizhou <kezizhou@gmail.com>
2019-12-30 20:16:59 -0500

    Removing line return

--- root/include_functions.php
+++ root/include_functions.php
@@ -9,13 +9,20 @@
 
         if( !isset($conn) ) {
             // Get Docker secret paths and values from the files
-            $strServerName = file_get_contents( getenv('mySQLServerNameFile', true) );
-            $strUsername= file_get_contents( getenv('mySQLUsernameFile', true) );
-            $strPassword = file_get_contents( getenv('mySQLPasswordFile', true) );
-            $strDBName = file_get_contents( getenv('mySQLDBNameFile', true) );
+            $astrConfig = array();
+            $astrConfig['servername'] = file_get_contents( getenv('mySQLServerNameFile', true) );
+            $astrConfig['username'] = file_get_contents( getenv('mySQLUsernameFile', true) );
+            $astrConfig['password'] = file_get_contents( getenv('mySQLPasswordFile', true) );
+            $astrConfig['dbname'] = file_get_contents( getenv('mySQLDBNameFile', true) );
+
+            // Need to remove line return in files
+            $astrConfig['servername'] = str_replace( array("\r", "\n"), '', $astrConfig['servername'] );
+            $astrConfig['username'] = str_replace( array("\r", "\n"), '', $astrConfig['username'] );
+            $astrConfig['password'] = str_replace( array("\r", "\n"), '', $astrConfig['password'] );
+            $astrConfig['dbname'] = str_replace( array("\r", "\n"), '', $astrConfig['dbname'] );
 
             // Create connection
-            $conn = mysqli_connect( $strServerName, $strUsername, $strPassword, $strDBName );
+            $conn = mysqli_connect($astrConfig['servername'], $astrConfig['username'], $astrConfig['password'], $astrConfig['dbname']);
         }
 
         // Check connection
